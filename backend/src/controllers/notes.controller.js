007a5b3a8f57e5ad5beb1a9cce8e9e51d0c5db4d
Esdras Clother <esdras.clother@outlook.com>
2021-02-02 22:56:20 -0600

    agregando models de la base de datos y usando schema, modificaciones en los controladores

--- backend/src/controllers/notes.controller.js
+++ backend/src/controllers/notes.controller.js
@@ -1,10 +1,42 @@
 const notesCtrl = {};
+const Note = require('../models/Notes');
 
-notesCtrl.getNotes = (req, res) => res.json({message: []});
-notesCtrl.createNotes = (req, res) => res.json({message: 'Notes Saved'});
+notesCtrl.getNotes = async (req, res) => {
+    const notes = await Note.find();
+    res.json(notes);
+} 
 
-notesCtrl.getNote = (req, res) => res.json({message: []});
-notesCtrl.updateNote = (req, res) => res.json({message: 'Notes Update'});
-notesCtrl.deleteNote = (req, res) => res.json({message: 'Notes Deleted'});
+notesCtrl.createNotes = async (req, res) => {
+    const {title, content, date, author} = req.body;
+    const newNote = new Note({
+        title: title,
+        content: content,
+        date: date,
+        author: author
+    });
+    await newNote.save();
+    res.json({message: 'Notes Saved'})
+};
+
+notesCtrl.getNote = async (req, res) => {
+    const note = await Note.findById(req.params.id);
+    console.log(note);
+    res.json(note)
+};
+
+notesCtrl.updateNote = async (req, res) => {
+    const {title, content, date, author} = req.body;
+    await Note.findOneAndUpdate({_id: req.params.id}, {
+        title: title,
+        author: author,
+        content: content
+    });
+    res.json({message: 'Notes Update'})
+};
+
+notesCtrl.deleteNote = async (req, res) => {
+    await Note.findOneAndDelete(req.params.id)
+    res.json({message: 'Notes Deleted'})
+};
 
 module.exports = notesCtrl;
