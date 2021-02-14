e7ec5e5257cffdef3b149a166616e573325603de
Esdras Clother <esdras.clother@outlook.com>
2021-02-13 19:23:18 -0600

    Terminando el proyecto con la finalizacion de poder actualizar nuestras notas

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -10,7 +10,9 @@ export default class CreateNote extends Component {
         userSelected: '',
         title: '',
         content: '',
-        date: new Date()
+        date: new Date(),
+        editing: false,
+        _id: ''
     }
 
     async componentDidMount() {
@@ -19,6 +21,17 @@ export default class CreateNote extends Component {
             users: res.data.map(user => user.username),
             userSelected: res.data[0].username
         })
+        if (this.props.match.params.id) {
+            const res = await axios.get('http://localhost:4000/api/notes/' + this.props.match.params.id);
+            this.setState({
+                title: res.data.title,
+                content: res.data.content,
+                date: new Date(res.data.date),
+                userSelected: res.data.author,
+                editing: true,
+                _id: this.props.match.params.id
+            })
+        }
     }
 
     onSubmit = async (e) => {
@@ -29,7 +42,11 @@ export default class CreateNote extends Component {
             content: this.state.content,
             date: this.state.date
         };
-        await axios.post('http://localhost:4000/api/notes', newNote);
+        if (this.state.editing) {
+            await axios.put('http://localhost:4000/api/notes/' + this.state._id, newNote);
+        } else {
+            await axios.post('http://localhost:4000/api/notes', newNote);
+        }
         window.location.href = '/';
     }
 
@@ -55,6 +72,7 @@ export default class CreateNote extends Component {
                             className="form-control" 
                             name="userSelected"
                             onChange={this.onInputChange}
+                            value={this.state.userSelected}
                         >
                             {
                               this.state.users.map(user =>  
@@ -72,6 +90,7 @@ export default class CreateNote extends Component {
                             placeholder="Title" 
                             name="title"
                             onChange={this.onInputChange}
+                            value={this.state.title}
                             required/>
                     </div>
 
@@ -81,6 +100,7 @@ export default class CreateNote extends Component {
                             className="form-control"
                             placeholder="Content"
                             onChange={this.onInputChange}
+                            value={this.state.content}
                             required>
 
                         </textarea>
